9496a4670a4a15749c4b8624f45db64e2efcc848
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2022-10-06 17:48:58 +0200

    aggiunto css

--- js/script.js
+++ js/script.js
@@ -4,39 +4,31 @@ let myTitolo = document.getElementById('titolo');
 myTitolo.innerHTML = `Benvenuto ${nomeUtente}!`;
 
 let kmDaPercorrere = parseFloat(prompt('Quanti Km hai intenzione di percorrere?')).toFixed(2);
+let kmDaPercorrereNumb = parseFloat(kmDaPercorrere);
 
 if(isNaN(kmDaPercorrere)){
     alert('Puoi inserire solo numeri! Ricarica la pagina e riprova')
 }
-let etaPasseggero = parseInt(prompt('Quanti anni hai?'));
-// console.log(kmDaPercorrere);
-// console.log(etaPasseggero);
 
-let kmDaPercorrereNumb = parseFloat(kmDaPercorrere);
-// console.log(kmDaPercorrereNumb);
+let etaPasseggero = parseInt(prompt('Quanti anni hai?'));
 
 let tariffaKm = 0.21;
-// console.log(tariffaKm);
 
 let prezzoKmTot = parseFloat(kmDaPercorrereNumb * tariffaKm).toFixed(2);
-
 let prezzoKmTotNumb = parseFloat(prezzoKmTot);
-console.log(prezzoKmTotNumb);
+
 
 if(etaPasseggero < 18){
-    let prezzoFinale = parseFloat(prezzoKmTotNumb * 80 / 100).toFixed(2);
-    // console.log(prezzoFinale);
-    document.getElementById('my_paragraph').innerHTML = `Importo Effettivo: ${prezzoKmTotNumb}€`;
-    document.getElementById('my_secretparagraph').innerHTML = `Importo bonus minori: ${prezzoFinale}€`;
+    let prezzoFinale = parseFloat(prezzoKmTotNumb * 80 / 100).toFixed(2); 
+    document.getElementById('my_paragraph').innerHTML = `Importo Precedente: ${prezzoKmTotNumb}€`;
+    document.getElementById('my_secretparagraph').innerHTML = `Importo bonus Under18: <span class="mySpan">${prezzoFinale}€</span>`;
 } else if(etaPasseggero > 65){
     let prezzoFinale = parseFloat(prezzoKmTotNumb * 60 / 100).toFixed(2);
-    // console.log(prezzoFinale);
-    document.getElementById('my_paragraph').innerHTML = `Importo Effettivo: ${prezzoKmTotNumb}€`;
-    document.getElementById('my_secretparagraph').innerHTML = `Importo bonus Over 65: ${prezzoFinale}€`;
+    document.getElementById('my_paragraph').innerHTML = `Importo Precedente: ${prezzoKmTotNumb}€`;
+    document.getElementById('my_secretparagraph').innerHTML = `Importo bonus Over 65: <span class="mySpan">${prezzoFinale}€</span>`;
 } else {
     let prezzoFinale = parseFloat(prezzoKmTotNumb).toFixed(2);
-    document.getElementById('my_paragraph').innerHTML = `Importo Totale: ${prezzoFinale}€`;
-    // console.log(parseFloat(prezzoKmTotNumb).toFixed(2));
+    document.getElementById('my_secretparagraph').innerHTML = `Importo Totale: <span class="mySpan">${prezzoFinale}€</span>`;
 }
 
 
